05c2cab437d2b3bedacfd53cd0fbeacd1452e64d
sabyasachi sen <sabyasachi237@gmail.com>
2022-09-17 15:38:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-linktree
+This is a linktree-style portfolio website with HTML and CSS.
---
+++ script.js
@@ -0,0 +1,12 @@
+const linkInfo={
+    "github":"https://github.com/sabyasachi237",
+    "linkedin":"https://www.linkedin.com/in/sabyasachi-sen-4954491b6",
+    "twitter":"https://twitter.com/ssen5271",
+    "facebook":"https://www.facebook.com/aarjun.sing.54",
+    "mail":"mailto: sabyasachisen237@gmail.com",
+}
+
+
+const onHandleClick = (link) => {
+    window.location.href = linkInfo[link]
+}
